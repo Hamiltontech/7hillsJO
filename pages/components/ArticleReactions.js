667e7324f5fabf7cfc903fe80f19d5071f2c9fa8
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-01-15 17:25:44 +0300

    anyhing

--- pages/components/ArticleReactions.js
+++ pages/components/ArticleReactions.js
@@ -1,165 +1,97 @@
-import React from "react";
-import Image from "next/image";
-import sky from "../../public/Images/placeholder.png";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ArticleReactions = ({ data, postID, setReactions, reactions, error, setError, type, setType }) => {
-const [numberOfReactions, setNumberOfReactions] = useState([])
+    const [numberOfReactions, setNumberOfReactions] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const token = "your token here";
 
-console.log(reactions)
-
-useEffect(()=>{
-
-reactions?.map((item)=>{
-  if(item?.id === postID){
-    setNumberOfReactions(item?.attributes?.reactions?.data)
-  }
-})
-}, [])
-
-
-const handleSubmit =  () => {
-      // make the post request to the strapi api to create the reaction
-      axios.post('https://strapi-104357-0.cloudclusters.net/api/reactions', {
-        "data": {
-          "Reaction": type || "",  
-          "article": postID || "",
+    useEffect(() => {
+        if(reactions && Array.isArray(reactions)){
+            const currentReactions = reactions.find(r => r.id === postID);
+            if(currentReactions){
+                setNumberOfReactions(currentReactions.attributes.reactions.data);
+                setLoading(false);
+            }else{
+                setLoading(false);
+            }
+        }else{
+            setLoading(false);
         }
-      }).then((response)=>{
-        setReactions(response.data.data)  
-        // setReactions(response.data.reactions); 
-      }).catch((error)=>{
+    }, [reactions, postID])
+
+    const handleSubmit = () => {
+        setLoading(true);
+        axios
+        .post("https://strapi-104357-0.cloudclusters.net/api/reactions", {
+            "data": {
+                "Reaction": type || "",  
+                "article": postID || "",
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        },{timeout: 30000}).then((response) => {
+            if(response.status === 200){
+            setReactions(response.data);
+            setType("")
+            setLoading(false);
+        }else if(response.status === 400){
+            console.log("An error occured: ", response.data)
+            setLoading(false);
+        }
+    }).catch((error) => {
         console.log(error)
-      })
-}
+        setLoading(false);
+    });
+};
+``
 
-if (error) {
-  return <p>An error occured: {error.message}</p>
-}
 
-if (!numberOfReactions) {
-  return <p>Loading...</p> 
+  const likeCount = numberOfReactions.filter(r => r.attributes.Reaction === 'like').length
+  const loveCount = numberOfReactions.filter(r => r.attributes.Reaction === 'love').length
+  const laughCount = numberOfReactions.filter(r => r.attributes.Reaction === 'laugh').length
   
-} 
- 
 
-console.log(numberOfReactions)
+    if (error) {
+        return <p>An error occured: {error.message}</p>
+    }
 
-  return (
-    <div className="grid place-items-center text-light/80 mt-10">
-      <h1 className="font-bold">What do you think?</h1>
-      <h1 className="font-extralight">No. Responses</h1>
-      <div className="grid grid-cols-2 lg:grid-cols-7 gap-2 mt-4">
+    if (loading) {
+        return <p>Loading...</p> 
+    } 
 
-{reactions?.reactions?.data?.map((item)=>{
-  return(
-    <>
-    <h1>{item?.attributes?.Reaction}</h1> 
-    </>
-  )
-}
-)} 
+    return (
+        <div className="grid place-items-center text-light/80 mt-10">
+            <h1 className="font-bold">What do you think?</h1>
+            <h1 className="font-extralight">No. Responses</h1>
+            <div className="grid grid-cols-2 lg:grid-cols-7 gap-2 mt-4">
+              <form onSubmit={handleSubmit}>
+    <select value={type} onChange={e => setType(e.target.value)}>
+        <option value="like">Like</option>
+        <option value="love">Love</option>
+        <option value="laugh">Laugh</option>
+        <option value="dislike">Dislike</option>
+    </select>
+    <button type="submit">Submit</button>
+</form>
+{/* ADD THE IMAGE */}
+<h1 className="px-3 font-semibold">{likeCount}</h1>
 
-        {/* like */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button onClick={()=> {handleSubmit(); setType("like")}}>
-            <img
-              src={
-                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/upvote-512x512.png"
-              }
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">{numberOfReactions?.filter(r => r?.attributes?.Reaction === 'like').length}</h1>
-        </div>
 
-        {/* love */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button onClick={()=> {handleSubmit(); setType("love")}}>
-            <img
-              src={
-                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/love-512x512.png"
-              }
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">
-          {numberOfReactions?.filter(r => r?.attributes?.Reaction === 'love').length} 
-
-          </h1>
-        </div>
-
-        {/* laugh */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button onClick={()=> {handleSubmit(); setType("happy")}}>
-            <img
-              src={"https://cdn-icons-png.flaticon.com/512/1933/1933576.png"}
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">
-            
-          {numberOfReactions?.filter(r => r?.attributes?.Reaction === 'happy').length} 
-            
-            </h1>
-        </div>
-
-        {/* shocked */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button>
-            <img
-              src={
-                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/surprised-512x512.png"
-              }
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">{numberOfReactions?.filter(r => r?.attributes?.Reaction === 'shock').length} </h1>
-        </div>
-
-        {/* angry */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button>
-            <img
-              src={
-                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/angry-512x512.png"
-              }
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">{numberOfReactions?.filter(r => r?.attributes?.Reaction === 'angry').length} </h1>
-        </div>
-
-        {/* clapping */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button>
-            <img
-              src={"https://cdn-icons-png.flaticon.com/512/1629/1629881.png"}
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">{numberOfReactions?.filter(r => r?.attributes?.Reaction === 'clap').length} </h1>
-        </div>
-
-        {/* sad */}
-        <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-          <button>
-            <img
-              src={
-                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/sad-512x512.png"
+                {numberOfReactions?.map((item)=>{
+                    return(
+                        <>
+                            <h1>{item?.attributes?.Reaction}</h1> 
+                        </>
+                    )
+                })} 
+                {/* like */}
+                <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
+                    <button onClick={()=> {handleSubmit(); setType("like")}}>
+                        <img
+                            src={
+                                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/sad-512x512.png"
               }
               width={45}
               height={40}
