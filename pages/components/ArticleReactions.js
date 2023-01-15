f411c8b913862d24ba0da7c22b0d40dde47d5eb0
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-01-15 18:03:54 +0300

    latest version

--- pages/components/ArticleReactions.js
+++ pages/components/ArticleReactions.js
@@ -19,38 +19,56 @@ const ArticleReactions = ({ data, postID, setReactions, reactions, error, setErr
             setLoading(false);
         }
     }, [reactions, postID])
-
-    const handleSubmit = () => {
-        setLoading(true);
-        axios
-        .post("https://strapi-104357-0.cloudclusters.net/api/reactions", {
-            "data": {
-                "Reaction": type || "",  
-                "article": postID || "",
-            },
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        },{timeout: 30000}).then((response) => {
-            if(response.status === 200){
-            setReactions(response.data);
-            setType("")
-            setLoading(false);
-        }else if(response.status === 400){
-            console.log("An error occured: ", response.data)
-            setLoading(false);
+  const handleSubmit = () => {
+    setLoading(true);
+    axios
+    .post("https://strapi-104357-0.cloudclusters.net/api/reactions", {
+        "data": {
+        "Reaction": type || "",  
+        "article": postID || "",
+        },
+        headers: {
+        Authorization: `Bearer ${token}`
         }
-    }).catch((error) => {
+    })
+    .then((response) => {
+        if(response.status === 200){
+        setReactions(response.data);
+        setType("")
+        setLoading(false);
+        setNumberOfReactions(response.data.attributes.reactions.data);
+        }else if(response.status === 400){
+        console.log("An error occured: ", response.data)
+        setLoading(false);
+        }
+    })
+    .catch((error) => {
         console.log(error)
         setLoading(false);
     });
 };
-``
+
+useEffect(() => {
+    if(reactions && postID){
+        const currentReactions = reactions.find(r => r.id === postID);
+        if(currentReactions){
+            setNumberOfReactions(currentReactions.attributes.reactions.data);
+        }
+    }
+}, [reactions, postID])
 
 
   const likeCount = numberOfReactions.filter(r => r.attributes.Reaction === 'like').length
   const loveCount = numberOfReactions.filter(r => r.attributes.Reaction === 'love').length
   const laughCount = numberOfReactions.filter(r => r.attributes.Reaction === 'laugh').length
+  const dislikeCount = numberOfReactions.filter(r => r.attributes.Reaction === 'dislike').length
+  const clapCount = numberOfReactions.filter(r => r.attributes.Reaction === 'clap').length
+  const sadCount = numberOfReactions.filter(r => r.attributes.Reaction === 'sad').length
+  const shockCount = numberOfReactions.filter(r => r.attributes.Reaction === 'shock').length
+  const angryCount = numberOfReactions.filter(r => r.attributes.Reaction === 'angry').length
+
+
+
   
 
     if (error) {
@@ -64,44 +82,41 @@ const ArticleReactions = ({ data, postID, setReactions, reactions, error, setErr
     return (
         <div className="grid place-items-center text-light/80 mt-10">
             <h1 className="font-bold">What do you think?</h1>
-            <h1 className="font-extralight">No. Responses</h1>
+            <h1 className="font-extralight">Number of Responses</h1>
             <div className="grid grid-cols-2 lg:grid-cols-7 gap-2 mt-4">
-              <form onSubmit={handleSubmit}>
-    <select value={type} onChange={e => setType(e.target.value)}>
-        <option value="like">Like</option>
-        <option value="love">Love</option>
-        <option value="laugh">Laugh</option>
-        <option value="dislike">Dislike</option>
-    </select>
-    <button type="submit">Submit</button>
+            <form>
+            <div className="reaction-buttons">
+    <button type="button" onClick={() => {setType("like"); handleSubmit()}}>Like</button>
+    <button type="button" onClick={() => {setType("love"); handleSubmit()}}>Love</button>
+    <button type="button" onClick={() => {setType("laugh"); handleSubmit()}}>Laugh</button>
+    <button type="button" onClick={() => {setType("dislike"); handleSubmit()}}>Dislike</button>
+    <button type="button" onClick={() => {setType("clap"); handleSubmit()}}>Clap</button>
+    <button type="button" onClick={() => {setType("sad"); handleSubmit()}}>Sad</button>
+    <button type="button" onClick={() => {setType("shock"); handleSubmit()}}>Shock</button>
+    <button type="button" onClick={() => {setType("angry"); handleSubmit()}}>Angry</button>
+</div>
+
+
 </form>
 {/* ADD THE IMAGE */}
-<h1 className="px-3 font-semibold">{likeCount}</h1>
+<h1 className="px-3 font-semibold">Likes {likeCount}</h1>
+<h1 className="px-3 font-semibold">Love {loveCount}</h1>
+<h1 className="px-3 font-semibold">Laugh {laughCount}</h1>
+<h1 className="px-3 font-semibold">Dislike {dislikeCount}</h1>
+<h1 className="px-3 font-semibold">Clap {clapCount}</h1>
+<h1 className="px-3 font-semibold">Sad {sadCount}</h1>
+<h1 className="px-3 font-semibold">Shock {shockCount}</h1>
+<h1 className="px-3 font-semibold">Angry {angryCount}</h1>
 
 
-                {numberOfReactions?.map((item)=>{
-                    return(
-                        <>
-                            <h1>{item?.attributes?.Reaction}</h1> 
-                        </>
-                    )
-                })} 
-                {/* like */}
-                <div className="bg-[#F8F8FF] rounded-full flex justify-between items-center px-2 py-1 hover:bg-blackk/20">
-                    <button onClick={()=> {handleSubmit(); setType("like")}}>
-                        <img
-                            src={
-                                "https://c.disquscdn.com/next/current/publisher-admin/assets/img/emoji/sad-512x512.png"
-              }
-              width={45}
-              height={40}
-              className="hover:scale-110 ease-in-out duration-150 cursor-pointer"
-            />
-          </button>
-          <h1 className="px-3 font-semibold">{numberOfReactions?.filter(r => r?.attributes?.Reaction === 'sad').length} </h1>
+
+
+
+
+
+              
         </div>
       </div>
-    </div>
   );
 };
 
